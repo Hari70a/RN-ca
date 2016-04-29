ab7784948341babec0c7a2c1dc55a0b20f6eda2b
Hariraj <raja70a@gmail.com>
2016-04-29 18:06:19 +0530

    Initial Commit

---
+++ RNCalendarManager/RNCalendarManager.h
@@ -0,0 +1,13 @@
+//
+//  RNCalendarManager.h
+//  RNCalendarManager
+//
+//  Created by Ranjith Varma on 29/04/16.
+//  Copyright © 2016 Spritle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCalendarManager : NSObject
+
+@end
---
+++ RNCalendarManager/RNCalendarManager.m
@@ -0,0 +1,13 @@
+//
+//  RNCalendarManager.m
+//  RNCalendarManager
+//
+//  Created by Ranjith Varma on 29/04/16.
+//  Copyright © 2016 Spritle. All rights reserved.
+//
+
+#import "RNCalendarManager.h"
+
+@implementation RNCalendarManager
+
+@end
